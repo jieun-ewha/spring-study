50912d20775f1e4097991ee39ec9210a16ce9ab2
hiwon-lee <84234411+hiwon-lee@users.noreply.github.com>
2025-08-19 11:38:25 +0900

    feat : 3주차 - 웹 UI, JPA도입

---
+++ 이희원/demo/src/main/java/com/example/demo/SpringConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import com.example.demo.repository.JpaMemberRepository;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+import com.example.demo.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+}
