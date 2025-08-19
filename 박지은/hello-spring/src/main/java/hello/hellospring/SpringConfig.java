627bcf107c9c9536138abccec9eaaec1943ac32f
jieun-ewha <2476124@ewha.ac.kr>
2025-08-19 13:58:44 +0900

    Add files via upload

---
+++ 박지은/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,31 @@
+package hello.hellospring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
+
+@Configuration
+public class SpringConfig {
+    
+    private final EntityManager em;
+    
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+    
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+    
+    @Bean
+    public MemberRepository memberRepository() {
+        // return new MemoryMemberRepository();
+        // return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+    }
+}
